1816b59823b61545599e50087c34d804da0c4cb2
pjenvey <none@none>
2007-01-27 14:00:46 -0800

    [svn] update paste.app_install entry point to use PylonsInstaller. (Removes the cheetah requirement for paster make-config)
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url="http://www.pylonshq.com/docs/quick_wiki.html",
     author="James Gardner",
     #author_email="",
-    install_requires=["Pylons>=0.9.4.1", "docutils==0.4", "SQLAlchemy>=0.2.6"],
+    install_requires=["Pylons==dev,>=0.9.4.1dev-r1772", "docutils==0.4", "SQLAlchemy>=0.2.6"],
     packages=find_packages(),
     include_package_data=True,
     test_suite = 'nose.collector',
@@ -16,6 +16,6 @@ setup(
     [paste.app_factory]
     main=quickwiki:make_app
     [paste.app_install]
-    main=paste.script.appinstall:Installer
+    main=pylons.util:PylonsInstaller
     """,
 )
