279112b056b6e05fba1055e8a965fbe6b73ff8b2
Philip Jenvey <pjenvey@underboss.org>
2009-02-19 00:04:22 -0800

    sort imports
    --HG--
    branch : trunk

--- quickwiki/lib/helpers.py
+++ quickwiki/lib/helpers.py
@@ -3,7 +3,7 @@
 Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to templates as 'h'.
 """
-from webhelpers.html import escape, HTML, literal, url_escape
+from webhelpers.html import HTML, escape, literal, url_escape
 from webhelpers.html.tags import *
 from webhelpers.html.secure_form import secure_form
 from webhelpers.pylonslib import Flash as _Flash
